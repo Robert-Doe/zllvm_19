e8618dcd44df7d0102d669c18c4d2eec847ae906
bobcumulus <evangelistebo@yahoo.com>
2024-03-31 23:02:31 -0700

    First

---
+++ libcxx/test/std/thread/thread.semaphore/max.pass.cpp
@@ -0,0 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// UNSUPPORTED: no-threads
+// UNSUPPORTED: c++03, c++11
+
+// <semaphore>
+
+#include <semaphore>
+#include <thread>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+  //from zeno: delete
+  //static_assert(std::counting_semaphore<>::max() >= 1, "");
+
+  static_assert(std::counting_semaphore<>::max() > 0, "");
+  static_assert(std::counting_semaphore<1>::max() >= 1, "");
+  //from zeno: delete
+  // static_assert(std::counting_semaphore<std::numeric_limits<int>::max()>::max() >= std::numeric_limits<int>::max(), "");
+  // static_assert(std::counting_semaphore<std::numeric_limits<std::ptrdiff_t>::max()>::max() == std::numeric_limits<ptrdiff_t>::max(), "");
+  static_assert(std::counting_semaphore<std::numeric_limits<int>::max()>::max() >= 1, "");
+  static_assert(std::counting_semaphore<std::numeric_limits<ptrdiff_t>::max()>::max() >= 1, "");
+  static_assert(std::counting_semaphore<1>::max() == std::binary_semaphore::max(), "");
+  return 0;
+}
